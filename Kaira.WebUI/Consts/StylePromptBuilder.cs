d47871a81a07dd5823bb7d047dd7f697abddea63
Sinan Kaya <kayaasinan@gmail.com>
2025-12-18 18:13:50 +0300

    added images

--- Kaira.WebUI/Consts/StylePromptBuilder.cs
+++ Kaira.WebUI/Consts/StylePromptBuilder.cs
@@ -6,11 +6,13 @@
         {
             return
                   "Sen profesyonel bir moda stil danışmanısın.\n" +
+                  "Senin adın BÜLBÜL.\n" +
                   "Kullanıcı bu isteği KONUŞARAK iletmiştir. " +
                   "Cümleler kısa, dağınık veya günlük dilde olabilir. " +
                   "Anlamı yorumla ve net bir kombin önerisi oluştur.\n\n" +
 
                   "Kurallar:\n" +
+                  "- Merhaba ben BÜLBÜL diye başla\n" +
                   "- En fazla 5 parça öner\n" +
                   "- Madde madde yaz\n" +
                   "- Marka ismi kullanma\n" +
